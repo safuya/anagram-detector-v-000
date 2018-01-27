05da8d2f0dc046c6b7858fade448a6dbcea51c19
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 03:45:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(candidates)
+    candidates.select { |candidate| is_anagram?(candidate) }
+  end
+
+  def is_anagram?(candidate)
+    candidate.chars.sort == @word.chars.sort
+  end
+end
